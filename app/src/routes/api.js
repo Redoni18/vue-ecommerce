6447b77d7a9ed0f104f51b22b6749ff150647821
RilindSylejmani <rs52543@ubt-uni.net>
2023-03-05 11:32:57 +0100

    Backend for Upcoming Products

--- app/src/routes/api.js
+++ app/src/routes/api.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 
@@ -27,6 +26,13 @@ router.get('/api/category/:id', categoryController.get_category);
 router.post('/api/uploadCategory', categoryController.upload_category);
 router.put('/api/editCategory/:id', categoryController.edit_category);
 router.delete('/api/categories/delete/:id', categoryController.delete_category);
+
+//Upcoming
+router.get('/api/upcomings', upcomingController.get_upcomings);
+router.get('/api/upcoming/:id', upcomingController.get_upcoming);
+router.post('/api/uploadUpcoming', upcomingController.upload_upcoming);
+router.put('/api/editUpcoming/:id', upcomingController.edit_upcoming);
+router.delete('/api/upcomings/delete/:id', upcomingController.delete_upcoming);
 
 
 module.exports = router;
