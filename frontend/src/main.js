031db411adddc34ab8fbdae1947bcbc1a5ca1296
Redon Emini <redon@cryptosphere-systems.com>
2023-03-08 01:42:05 +0100

    changes in node module packages and added logout function in user view navbar

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import BootstrapVueNext from 'bootstrap-vue-next'
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 import { initializeApp } from "firebase/app";
@@ -22,11 +23,6 @@ import 'vue-good-table-next/dist/vue-good-table-next.css'
 
 import 'vue-select/dist/vue-select.css';
 import vSelect from 'vue-select'
-import { CAlert } from '@coreui/vue';
-import '@coreui/coreui/dist/css/coreui.min.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'jquery/src/jquery.js'
-import 'bootstrap/dist/js/bootstrap.min.js'
 
 var firebaseConfig = {
     apiKey: "AIzaSyDjhzE9aaeDQw46_iWSLHLOek6TzP6QlsI",
@@ -42,4 +38,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 export { app, auth }
 
-createApp(App).use(store).use(router).use(BootstrapVueNext).use(Vue3Toasity).use(VueGoodTablePlugin).use(VueSidebarMenu).component('v-select', vSelect).use(CAlert).mount('#app')
+createApp(App).use(store).use(router).use(BootstrapVueNext).use(Vue3Toasity).use(VueGoodTablePlugin).use(VueSidebarMenu).component('v-select', vSelect).mount('#app')
