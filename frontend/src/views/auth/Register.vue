a56c2e3862c611b908d0bbf3fdef82700c4544aa
RilindSylejmani <rs52543@ubt-uni.net>
2023-03-09 00:54:04 +0100

    added functionality for register

--- frontend/src/views/auth/Register.vue
+++ frontend/src/views/auth/Register.vue
@@ -7,25 +7,27 @@
                     <div class="row no-gutters">
                         <div class="col-lg-6">
                             <div class="p-5">
-                                <div class="mb-5">
-                                    <h3 class="h4 font-weight-bold text-theme">Register</h3>
-                                </div>
-                                <h6 class="h5 mb-0">Just Do Register.</h6>
-                                <p class="text-muted mt-2 mb-5">If You Really Want To Know, Look In The Register.</p>
-                                <form>
+                                <h4 class="h3 mb-30" style="color:crimson">Register</h4>
+                                <br>
+                                <form @submit="registerUser">
                                     <div class="form-group">
                                         <label for="yourName">Your name</label>
-                                        <input type="text" class="form-control" id="yourName" />
+                                        <input type="text" class="form-control" id="yourName" v-model="fullname" required/>
                                     </div>
+                                    <br>
                                     <div class="form-group">
                                         <label for="exampleInputEmail1">Email address</label>
-                                        <input type="email" class="form-control" id="exampleInputEmail1" />
+                                        <input type="email" class="form-control" id="exampleInputEmail1" v-model="email" required/>
                                     </div>
+                                    <br>
                                     <div class="form-group mb-5">
                                         <label for="exampleInputPassword1">Password</label>
-                                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                                        <input type="password" class="form-control" id="exampleInputPassword1" v-model="password" required/>
                                     </div>
-                                    <button type="submit" class="btn btn-theme">Register</button>
+                                    <!-- <button type="submit" class="btn btn-theme">Register</button> -->
+                                    <div class="d-grid gap-2">
+                                      <button type="submit" class="btn btn-theme">Register</button>
+                                    </div>
                                 </form>
                             </div>
                         </div>
@@ -33,10 +35,7 @@
                             <div class="account-block rounded-right">
                                 <div class="overlay rounded-right"></div>
                                 <div class="account-testimonial">
-                                    <!-- <h4 class="text-white mb-4">This beautiful theme yours!</h4>
-                                    <p class="lead text-white">"Best investment i made for a long time. Can only recommend it for other users."</p>
-                                    <p>- Admin User</p> -->
-                                    <img src="https://png.pngtree.com/png-clipart/20220921/original/pngtree-register-now-label-psd-png-image_8625922.png" width="460px" height="460px" alt="">
+                                    <!-- <img src="" alt=""> -->
                                 </div>
                             </div>
                         </div>
@@ -45,107 +44,48 @@
                 <!-- end card-body -->
             </div>
             <!-- end card -->
-            <p class="text-muted text-center mt-3 mb-0">Already have an account? <a href="login.html" style="color:crimson" class="text-primary ml-1">login</a></p>
+                 <p class="text-muted text-center mt-3 mb-0">Already have an account? <span class="text-primary ml-1" @click="goToSignIn">Login!</span></p>
             <!-- end row -->
         </div>
         <!-- end col -->
     </div>
     <!-- Row -->
 </div>
-    <!-- <div class="registration">
-      <div class="mt-4 register-card">
-        <h2>Register below</h2>
-        <b-form class="mt-4" @submit="registerUser">
-          <b-form-group
-            id="input-group-1"
-            label="Full Name:"
-            label-cols-sm="4"
-            label-cols-lg="2"
-            content-cols-sm
-            content-cols-lg="10"
-            label-align="start"
-          >
-            <b-form-input
-              v-model="fullname"
-              placeholder="Full Name"
-              required
-            ></b-form-input>
-          </b-form-group>
-
-          <b-form-group
-            id="input-group-1"
-            label="Email:"
-            label-cols-sm="4"
-            label-cols-lg="2"
-            content-cols="10"
-            label-align="start"
-          >
-            <b-form-input
-              v-model="email"
-              type="email"
-              placeholder="Enter your email"
-              required
-            ></b-form-input>
-          </b-form-group>
-
-          <b-form-group
-            id="input-group-1"
-            label="Password:"
-            label-cols-sm="4"
-            label-cols-lg="2"
-            content-cols-sm
-            content-cols-lg="10"
-            label-align="start"
-          >
-            <b-form-input
-              v-model="password"
-              type="password"
-              placeholder="Enter your password"
-              required
-            ></b-form-input>
-          </b-form-group>
-          <p class="redirect-link">Already have an account? <span @click="goToSignIn">Sign In!</span></p>
-
-
-          <b-button type="submit" variant="light" class="mt-2">Register</b-button>
-        </b-form>
-    </div>
-    </div> -->
 </template>
 
 <script>
-// import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
-// import { insertUser } from '@/eCommerce-sdk/user.js'
-// export default {
-//   data() {
-//     return {
-//       uid: null,
-//       fullname: null,
-//       email: null,
-//       password: null,
-//     }
-//   },
-//   methods: {
-//     async registerUser() {
-//       const auth = getAuth();
-//       try{
-//         const response = await createUserWithEmailAndPassword(auth, this.email, this.password)
-//         await updateProfile(auth.currentUser, {displayName: this.fullname})
-//         this.$router.push({ path: "/login" })
-//       } catch(err) {
-//         console.log(err)
-//       } finally {
-//         this.registerUserWithRole({uid: auth.currentUser.uid, fullname: this.fullname, email: this.email})
-//       }
-//     },
-//     async registerUserWithRole(user) {
-//       await insertUser(user)
-//     },
-//     goToSignIn() {
-//       this.$router.push({ path: "/login"})
-//     }
-//   }
-// }
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { insertUser } from '@/eCommerce-sdk/user.js'
+export default {
+  data() {
+    return {
+      uid: null,
+      fullname: null,
+      email: null,
+      password: null,
+    }
+  },
+  methods: {
+    async registerUser() {
+      const auth = getAuth();
+      try{
+        const response = await createUserWithEmailAndPassword(auth, this.email, this.password)
+        await updateProfile(auth.currentUser, {displayName: this.fullname})
+        this.$router.push({ path: "/login" })
+      } catch(err) {
+        console.log(err)
+      } finally {
+        this.registerUserWithRole({uid: auth.currentUser.uid, fullname: this.fullname, email: this.email})
+      }
+    },
+    async registerUserWithRole(user) {
+      await insertUser(user)
+    },
+    goToSignIn() {
+      this.$router.push({ path: "/login"})
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss">
@@ -194,34 +134,5 @@ body {
     border-color: crimson;
     color: #fff;
 }
-// .registration {
-//   height: 100vh;
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-//   background: #A7BFE8;  /* fallback for old browsers */
-  
-//   .register-card {
-//     padding: 3%;
-//     width: 50%;
-//     box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
-//     border-radius: 20px;
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: center;
-//     background: #bbd1f7;
-//   }
 
-//   .redirect-link{
-//     text-align: center;
-//     font-size: 12px;
-//     color: #575656;
-
-//     span {
-//       color: #030303;
-//       cursor: pointer;
-//     }
-//   }
-// }
 </style>
