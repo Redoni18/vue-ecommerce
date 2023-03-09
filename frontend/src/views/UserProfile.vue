fe6d66ad08378eff40ea6cd910922eeaf521d063
vullnetk <vk53084@ubt-uni.net>
2023-03-09 02:39:31 +0100

    start user profile update

--- frontend/src/views/UserProfile.vue
+++ frontend/src/views/UserProfile.vue
@@ -40,51 +40,64 @@
               </div>
             </div>
             <div class="col-md-8">
-              <div class="card mb-3">
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Full Name</h6>
+            <b-form @submit="onSubmit">
+                <div class="card mb-3">
+                    <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-3">
+                        <h6 class="mb-0">Full Name</h6>
+                        </div>
+                        <div class="col-sm-9">
+                            <b-form-input
+                                v-model="currentUser.displayName"
+                                type="text"
+                                placeholder="Full Name"
+                                required
+                                :disabled="edit"
+                            ></b-form-input>
+                        </div>
                     </div>
-                    <div class="col-sm-9 text-secondary">
-                        {{currentUser.displayName}}
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                        <h6 class="mb-0">Email</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            <b-form-input
+                                v-model="currentUser.email"
+                                type="email"
+                                placeholder="Email"
+                                required
+                                disabled
+                            ></b-form-input>
+                        </div>
                     </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Email</h6>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                        <h6 class="mb-0">Phone</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            <b-form-input
+                                v-model="currentUser.phoneNumber"
+                                type="text"
+                                placeholder="Not provided "
+                                required
+                                disabled
+                            ></b-form-input>
+                        </div>
                     </div>
-                    <div class="col-sm-9 text-secondary">
-                        {{currentUser.email}}
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-12">
+                        <a class="btn btn-info " target="__blank" @click="editForm" v-if="edit === true">Edit</a>
+                        <a class="btn btn-info " target="__blank" @click="updateUser" v-if="edit !== true" >Update</a>
+                        </div>
                     </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Phone</h6>
                     </div>
-                    <div class="col-sm-9 text-secondary">
-                        {{!currentUser.phoneNumber ? "Not provided" : currentUser.phoneNumber}}
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3">
-                      <h6 class="mb-0">Address</h6>
-                    </div>
-                    <div class="col-sm-9 text-secondary">
-                        {{!currentUser.phoneNumber ? "Not provided" : currentUser.phoneNumber}}
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <a class="btn btn-info " target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit</a>
-                    </div>
-                  </div>
                 </div>
-              </div>
+            </b-form>
+              
             </div>
           </div>
 
@@ -95,13 +108,15 @@
 <script>
 
 import { mapGetters } from 'vuex'
+import { getAuth, updateProfile } from "firebase/auth";
 export default {
     name: 'UserProfile',
     components: {
     },
     data() {
     return {
-        currentUser : null
+        currentUser : null,
+        edit : true,
         }
     },
     computed: {
@@ -111,6 +126,23 @@ export default {
   },
   mounted(){
     this.currentUser = this.user.data.user.providerData[0]
+  },
+  methods: {
+    editForm(){
+        this.edit = !this.edit
+        console.log(this.edit)
+    },
+    async updateUser(){
+        this.edit = !this.edit
+        const auth = getAuth();
+
+        const updatedProfile = await updateProfile(auth.currentUser, {displayName: this.currentUser.displayName})
+        .then((response) => console.log(response))
+        .catch((err) => console.log(err))
+        
+        console.log('eeeej '+ updatedProfile)
+        this.$router.push({ path: "/profile" })
+    }
   }
   }
 
