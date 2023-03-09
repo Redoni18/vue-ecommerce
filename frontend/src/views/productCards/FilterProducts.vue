6e3bfb6dff722430892480be117815f585bf4381
RilindSylejmani <rs52543@ubt-uni.net>
2023-03-09 02:36:41 +0100

    Design for search in productCards

--- frontend/src/views/productCards/FilterProducts.vue
+++ frontend/src/views/productCards/FilterProducts.vue
@@ -1,5 +1,27 @@
 <template>
-    
+    <div class="container justify-content-left" style="max-width: 350px;">
+    <br/>
+	<div class="searchbar row justify-content-left">
+                        <div class="col-12 col-md-11 col-lg-11">
+                             <form class="card card-sm">
+                                <div class="card-body row no-gutters align-items-center" style="height:50px">
+                                    <div class="col-auto" margin-top="-10px">
+                                        <i class="fas fa-search h4 text-body"></i>
+                                    </div>
+                                    
+                                    <div class="col">
+                                        <input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Search Products" style="margin-top: -15px;">
+                                    </div>
+                                    
+                                    
+                                   
+                                </div>
+                            </form> 
+                        </div>
+                        <!--end of col-->
+                    </div>
+
+    <br>
     <div class="filters-sidebar">
         <h4 class="filter-title">Filter Products</h4>
 
@@ -10,7 +32,7 @@
             <label><input type="checkbox" class="icheck"> Design</label>
         </div>
         <div class="checkbox">
-            <label><input type="checkbox" class="icheck"> Desktop</label>
+            <label><input type="checkbox" class="icheck"> Desktop</label>zz
         </div>
         <div class="checkbox">
             <label><input type="checkbox" class="icheck"> Management</label>
@@ -19,7 +41,7 @@
             <label><input type="checkbox" class="icheck"> Mobile</label>
         </div>
     </div>
-        
+</div>
 </template>
 
 <script>
@@ -31,7 +53,7 @@ export default {
 <style lang="scss" scoped >
 
 .filters-sidebar {
-    width: 400px;
+    width: 300px;
 
     display: flex;
     flex-direction: column;
@@ -47,11 +69,31 @@ export default {
     }
 }
 
+
+
 @media only screen and (max-width: 900px) {
     .filters-sidebar {
         width: 100%;
         margin: auto;
     }
+}
+
+@media only screen and (max-width:900px) {
+    .searchbar {
+        width: 100%;
+        margin: auto;
+    }
+}
+
+
+.form-control-borderless {
+    border: none;
+}
+
+.form-control-borderless:hover, .form-control-borderless:active, .form-control-borderless:focus {
+    border: none;
+    outline: none;
+    box-shadow: none;
 }
 
 </style>
